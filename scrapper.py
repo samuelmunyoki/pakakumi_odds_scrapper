a596d832706d40a4eb8e2a803416d018890f498b
samuelmunyoki <samuelmunyoki@yahoo.com>
2023-07-11 12:49:37 +0300

    'Updates'

---
+++ scrapper.py
@@ -0,0 +1,46 @@
+import csv
+import asyncio
+import nest_asyncio
+from pyppeteer import launch
+from pyppeteer_stealth import stealth
+
+SELECTOR = '#root > div.css-ynz9y9 > div:nth-child(2) > div > div.css-1633bsf > div > div.css-1pcha61 > div > div.css-1633bsf > div > table > tbody>tr'
+OLD_ARRAY = []
+NEW_ARRY = []
+
+async def launch_webpage():
+    browser = await launch(headless=True)
+    page = await browser.newPage()
+    await stealth(page)
+    await page.goto('https://play.pakakumi.com/')
+    await page.waitForSelector(SELECTOR)
+    tr_elements = await page.querySelectorAll(SELECTOR)
+    try:
+        with open('output\pakakumi_odds.csv', 'a', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            OLD_ARRAY.extend(NEW_ARRY)
+            NEW_ARRY.clear()
+            for tr_element in tr_elements:
+                text_content = await page.evaluate('(trElement) => trElement.textContent', tr_element)
+                NEW_ARRY.append(text_content.replace('x___',''))
+            set1 = set(NEW_ARRY)
+            set2 = set(OLD_ARRAY)
+            new_odds = list(set1.difference(set2))
+            for odd in new_odds:
+                csv_writer.writerow(odd)
+    except:
+        pass
+    return browser
+async def main(runs):
+    try:
+        while runs > 0:
+            browser = await launch_webpage()
+            await asyncio.sleep(45)
+            await browser.close()
+            runs = runs-1
+    except:
+        pass
+nest_asyncio.apply()
+new_loop = asyncio.new_event_loop()
+asyncio.get_event_loop_policy().set_event_loop(new_loop)
+new_loop.run_until_complete(main(runs=10000))
